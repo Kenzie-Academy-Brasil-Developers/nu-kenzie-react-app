908375b9400d6a71eb6899defc2d9b35a014bb66
Gabriel <gabpavao@outlook.com>
2022-11-17 14:15:47 -0300

    feat: Figure HTML structure

--- nu-kenzie/src/App.jsx
+++ nu-kenzie/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import { Figure } from './components/Opening Page/Figure/figure'
 import { Section } from './components/Opening Page/Section/section'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Section/>
+      <Figure/>
     </div>
   )
 }
---
+++ nu-kenzie/src/components/Opening Page/Figure/figure.jsx
@@ -0,0 +1,7 @@
+export function Figure () {
+    return (
+        <figure>
+            <img src="/src/assets/figure.svg" alt="Demonstração do App" />
+        </figure>
+    )
+}
