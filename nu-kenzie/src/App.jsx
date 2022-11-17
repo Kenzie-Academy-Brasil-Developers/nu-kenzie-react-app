4c1fb0d39329411d89323da2493775d735b480e5
Gabriel <gabpavao@outlook.com>
2022-11-17 14:10:39 -0300

    feat: Section HTML structure in the opening page

--- nu-kenzie/src/App.jsx
+++ nu-kenzie/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import { Section } from './components/Opening Page/Section/section'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Section/>
     </div>
   )
 }
---
+++ nu-kenzie/src/components/Opening Page/Section/section.jsx
@@ -0,0 +1,11 @@
+
+export function Section () {
+    return (
+        <section>
+            <h1><span>Nu</span> Kenzie</h1>
+            <p>Centralize o controle das suas finanças</p>
+            <p>de forma rápida e segura</p>
+            <button>Iniciar</button>
+        </section>
+    )
+}
