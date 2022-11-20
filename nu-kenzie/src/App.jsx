9c356557c2600246f4e63a2cf18b8a255bebba7c
Gabriel <gabpavao@outlook.com>
2022-11-20 13:05:41 -0300

    feat

--- nu-kenzie/src/App.jsx
+++ nu-kenzie/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { Balance } from './components/Home Page/Balance List/balance'
 import { Form } from './components/Home Page/Form/form'
 import { Header } from './components/Home Page/Header/header'
+import { TotalValue } from './components/Home Page/Total Value/totalValue'
 import { Figure } from './components/Opening Page/Figure/figure'
 import { Section } from './components/Opening Page/Section/section'
 
@@ -12,19 +13,6 @@ function App() {
   function addBalanceData (newBalanceData) {
     setBalanceData([...balanceData, newBalanceData])
   }
-  
-  // const balanceData = [
-  //   {
-  //     description: 'Salário - Mês Dezembro',
-  //     value: 6000,
-  //     typeOfValue: 'Receita'
-  //   },
-  //   {
-  //     description: 'Compra - Camiseta Preta',
-  //     value: 60,
-  //     typeOfValue: 'Despesa'
-  //   }
-  // ]
   
   const [page, setPage] = useState(true)
 
@@ -46,7 +34,10 @@ function App() {
     (<div className='bg-white'>
       <Header homePage={homePage}/>
       <div className='container layout'>
-        <Form addBalanceData={addBalanceData}/>
+        <section>
+          <Form addBalanceData={addBalanceData}/>
+          <TotalValue/>
+        </section>
         <Balance balanceData={balanceData}/>
       </div>
     </div>)
---
+++ nu-kenzie/src/components/Home Page/Total Value/totalValue.jsx
@@ -0,0 +1,13 @@
+import './totalValue.css'
+
+export function TotalValue () {
+    return (
+        <div>
+            <div>
+                <h2>Valor total:</h2>
+                <span>R$ 500</span>
+            </div>
+            <p>O valor se refere ao saldo</p>
+        </div>
+    )
+}
