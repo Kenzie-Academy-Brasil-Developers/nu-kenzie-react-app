cfde0653d0a8e1d6e1dad3d5e98a4b8ad09a0de7
Gabriel <gabpavao@outlook.com>
2022-11-22 12:02:09 -0300

    feat: calculate total value

--- nu-kenzie/src/components/Home Page/Form/form.jsx
+++ nu-kenzie/src/components/Home Page/Form/form.jsx
@@ -11,6 +11,18 @@ export function Form ({ addBalanceData }) {
         typeOfValue: ''
     }])
 
+    // function setValue (event) {
+    //     console.log(event.target.value)
+    //     console.log(formData)
+    //     if (formData.typeOfValue == 'Receita') {
+    //         setFormData({...formData, value: event.target.value})
+
+    //     } else if (formData.typeOfValue == 'Despesa') {
+    //         setFormData({...formData, value: (event.target.value) * -1})
+
+    //     }
+    // }
+
     function submit (event) {
         event.preventDefault()
 
@@ -39,7 +51,7 @@ export function Form ({ addBalanceData }) {
 
                 <div>
                     <label htmlFor="typeOfValue">Tipo do Valor</label>
-                    <select defaultValue={formData.typeOfValue} onMouseMove={(event) => setFormData({...formData, typeOfValue: event.target.value})} name="typeOfValue" id="typeOfValue">
+                    <select defaultValue={formData.typeOfValue} onClickCapture={(event) => setFormData({...formData, typeOfValue: event.target.value})} name="typeOfValue" id="typeOfValue">
                         <option value="Receita">Receita</option>
                         <option value="Despesa">Despesa</option>
                     </select>
--- nu-kenzie/src/components/Home Page/Total Value/totalValue.jsx
+++ nu-kenzie/src/components/Home Page/Total Value/totalValue.jsx
@@ -1,11 +1,17 @@
 import './totalValue.css'
 
-export function TotalValue () {
+export function TotalValue ( {balanceData} ) {
     return (
         <div className='total-value'>
             <div>
                 <h2>Valor total:</h2>
-                <span>R$ 500</span>
+                <span>R$ {balanceData.reduce((previousValue, currentValue) => {
+                        if (currentValue.typeOfValue == 'Receita') {
+                            return Number(previousValue) + Number(currentValue.value)
+                        } else {
+                            return Number(previousValue) + (Number(currentValue.value) * -1)
+                        }
+                    }, 0)}</span>
             </div>
             <p>O valor se refere ao saldo</p>
         </div>
